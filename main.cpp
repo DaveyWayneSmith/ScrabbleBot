968a052e73b7bceeac31bcfbd4347f19539e3964
Davey Smith <DaveySmith14@gatech.edu>
2016-05-19 20:07:40 -0400

    Filled the racks, made the pile global

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 char board[15][15];
 char playerOneRack[7];
 char playerTwoRack[7];
+pile* testPile = new pile();
 
 int onStart() {
     for (int i = 0; i < 15; i++) {
@@ -12,17 +13,19 @@ int onStart() {
             board[i][j] = ' ';
         }
     }
-
-
+    for (int j = 0; j < 7; j++) {
+        playerOneRack[j] = testPile->draw();
+        playerTwoRack[j] = testPile->draw();
+    }
     return 0;
 };
 
 int main() {
     onStart();
-    pile* testPile = new pile();
     for (int i = 0; i < 102; i++) {
         char ch = testPile->draw();
         cout << ch;
     }
+    return 0;
 }
 
