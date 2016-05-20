fb8e32312ad070cce3880511cc7b9270cc52c923
Edward Foyle <efoyle3@gatech.edu>
2016-05-20 10:05:07 -0400

    added board and game classes

---
+++ game.h
@@ -0,0 +1,20 @@
+//
+// Created by Edward on 5/19/2016.
+//
+
+#ifndef SCRABBLEBOT_GAME_H
+#define SCRABBLEBOT_GAME_H
+
+#include "board.h"
+
+
+class game {
+    board gameBoard;
+    short currPlayer;
+public:
+    bool play(); // wait for player to play
+    void display(); // display the current state of the game
+};
+
+
+#endif //SCRABBLEBOT_GAME_H
